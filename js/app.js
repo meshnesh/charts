b6f3a9b3d70c0a85a223f627637583426f5c4b9c
meshnesh <tonnie.nesh@gmail.com>
2016-09-28 22:43:54 +0300

    added content on about

--- js/app.js
+++ js/app.js
@@ -1,1 +1,50 @@
-$(document).foundation()
+// ------------------------------
+// SMOOTH SCROLL
+// ------------------------------
+function scrollNav() {
+    $('.nav a').click(function () {
+        //Toggle Class
+        $(".active").removeClass("active");
+        $(this).closest('li').addClass("active");
+        var theClass = $(this).attr("class");
+        $('.' + theClass).parent('li').addClass('active');
+        //Animate
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 160
+        }, 400);
+        return false;
+    });
+    $('.scrollTop a').scrollTop();
+}
+scrollNav();
+// ------------------------------
+// SMOOTH SCROLL END
+// ------------------------------
+// ********************************************************
+// NAV BAR
+// 
+$(function () {
+    // Returns width of browser viewport
+    var browser = $(window).width();
+    // Returns width of HTML document
+    var document = $(document).width();
+    $('.nav .toggle-nav').click(function (e) {
+        $('.nav .nav-mobile').addClass('style-mobile').slideToggle('slow');
+        e.preventDefault();
+    });
+});
+// ********************************************************
+// NAV BAR END
+//
+// ------------------------------
+// SCROLLING EFFECT
+// ------------------------------
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+        $("nav").addClass("scrolling");
+    }
+    else {
+        $("nav").removeClass("scrolling");
+    }
+});
