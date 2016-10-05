967a4774f4907877c481dcc736536bc8db996600
meshnesh <tonnie.nesh@gmail.com>
2016-10-05 11:55:53 +0300

    modified the page nav

--- js/app.js
+++ js/app.js
@@ -1,25 +1,3 @@
-// ------------------------------
-// SMOOTH SCROLL
-// ------------------------------
-function scrollNav() {
-	$('.nav a').click(function () {
-		//Toggle Class
-		$(".active").removeClass("active");
-		$(this).closest('li').addClass("active");
-		var theClass = $(this).attr("class");
-		$('.' + theClass).parent('li').addClass('active');
-		//Animate
-		$('html, body').stop().animate({
-			scrollTop: $($(this).attr('href')).offset().top - 160
-		}, 400);
-		return false;
-	});
-	$('.scrollTop a').scrollTop();
-}
-scrollNav();
-// ------------------------------
-// SMOOTH SCROLL END
-// ------------------------------
 // ********************************************************
 jQuery(document).ready(function ($) {
 	// browser window scroll (in pixels) after which the "menu" link is shown
@@ -122,3 +100,48 @@ slides = document.getElementsByClassName('slide');
 currentSlide = 0;
 nextSlide = 1;
 formatText();
+// ********************************************************
+// ------------------------------
+// SMOOTH SCROLL
+// ------------------------------
+function scrollNav() {
+	$('.nav a').click(function () {
+		//Toggle Class
+		$(".active").removeClass("active");
+		$(this).closest('li').addClass("active");
+		var theClass = $(this).attr("class");
+		$('.' + theClass).parent('li').addClass('active');
+		//Animate
+		$('html, body').stop().animate({
+			scrollTop: $($(this).attr('href')).offset().top - 160
+		}, 400);
+		return false;
+	});
+	$('.scrollTop a').scrollTop();
+}
+scrollNav();
+// ------------------------------
+// SMOOTH SCROLL END
+// ------------------------------
+// ********************************************************
+// NAV BAR
+// 
+$(function () {
+	// Returns width of browser viewport
+	var browser = $(window).width();
+	// Returns width of HTML document
+	var document = $(document).width();
+	$('.nav .toggle-nav').click(function (e) {
+		$('.nav .nav-mobile').addClass('style-mobile').slideToggle('slow');
+		e.preventDefault();
+	});
+});
+$(window).scroll(function () {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 50) {
+		$("nav").addClass("scrolling");
+	}
+	else {
+		$("nav").removeClass("scrolling");
+	}
+});
