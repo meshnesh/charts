d4cde0e99a722e57b3dbb350ba70d83d99f9208f
meshnesh <tonnie.nesh@gmail.com>
2016-10-11 09:24:29 +0300

    changed the site content

--- js/app.js
+++ js/app.js
@@ -1,147 +1,108 @@
-// ********************************************************
-jQuery(document).ready(function ($) {
-	// browser window scroll (in pixels) after which the "menu" link is shown
-	var offset = 300;
-	var navigationContainer = $('#cd-nav')
-		, mainNavigation = navigationContainer.find('#cd-main-nav ul');
-	//hide or show the "menu" link
-	checkMenu();
-	$(window).scroll(function () {
-		checkMenu();
-	});
-	//open or close the menu clicking on the bottom "menu" link
-	$('.cd-nav-trigger').on('click', function () {
-		$(this).toggleClass('menu-is-open');
-		//we need to remove the transitionEnd event handler (we add it when scolling up with the menu open)
-		mainNavigation.off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend').toggleClass('is-visible');
-	});
-
-	function checkMenu() {
-		if ($(window).scrollTop() > offset && !navigationContainer.hasClass('is-fixed')) {
-			navigationContainer.addClass('is-fixed').find('.cd-nav-trigger').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function () {
-				mainNavigation.addClass('has-transitions');
-			});
-		}
-		else if ($(window).scrollTop() <= offset) {
-			//check if the menu is open when scrolling up
-			if (mainNavigation.hasClass('is-visible') && !$('html').hasClass('no-csstransitions')) {
-				//close the menu with animation
-				mainNavigation.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
-					//wait for the menu to be closed and do the rest
-					mainNavigation.removeClass('is-visible is-hidden has-transitions');
-					navigationContainer.removeClass('is-fixed');
-					$('.cd-nav-trigger').removeClass('menu-is-open');
-				});
-				//check if the menu is open when scrolling up - fallback if transitions are not supported
-			}
-			else if (mainNavigation.hasClass('is-visible') && $('html').hasClass('no-csstransitions')) {
-				mainNavigation.removeClass('is-visible has-transitions');
-				navigationContainer.removeClass('is-fixed');
-				$('.cd-nav-trigger').removeClass('menu-is-open');
-				//scrolling up with menu closed
-			}
-			else {
-				navigationContainer.removeClass('is-fixed');
-				mainNavigation.removeClass('has-transitions');
-			}
-		}
-	}
-});
-//SLIDER
-config = {
-	timeout: 4000
-	, speed: 190
-	, spread: 20
-}
-var currentSlide = 0
-	, nextSlide = 0
-	, output = ''
-	, characters = []
-	, maxChars, slides, slidesTexts;
-callback = function ()  {
-	if (currentSlide++ >= slides.length - 1) currentSlide = 0;
-	setTimeout(formatText, config.timeout);
-}
-formatText = function () {
-	// calculate the next slide index
-	nextSlide = (currentSlide + 1 >= slides.length) ? 0 : currentSlide + 1;
-	// Fill the characters array if not already defined
-	if (characters[currentSlide] == undefined) characters[currentSlide] = slides[currentSlide].innerHTML.split('');
-	if (characters[nextSlide] == undefined) characters[nextSlide] = slides[nextSlide].innerHTML.split('');
-	maxChars = Math.max(characters[currentSlide].length, characters[nextSlide].length);
-	var fragment = document.createDocumentFragment();
-	for (var i = 0; i < maxChars; ++i) {
-		// create all elements
-		var duration = config.speed / 1000;
-		var delay = i * (config.speed / 1000 / config.spread);
-		var front = document.createElement("span");
-		front.className = "front";
-		var styleFront = '-webkit-animation-delay: ' + delay + 's, ' + (delay + duration) + 's; -webkit-animation-duration: ' + duration + 's, 0.01s;' + '-moz-animation-delay: ' + delay + 's, ' + (delay + duration) + 's; -moz-animation-duration: ' + duration + 's, 0.01s;';
-		front.setAttribute('style', styleFront);
-		front.innerHTML = characters[currentSlide][i] || ' ';
-		var back = document.createElement("span");
-		back.className = "back";
-		var styleBack = '-webkit-animation-delay: ' + (delay + duration) + 's, ' + (delay + duration) + 's; -webkit-animation-duration: ' + duration + 's, 0.01s;' + '-moz-animation-delay: ' + (delay + duration) + 's, ' + (delay + duration) + 's; -moz-animation-duration: ' + duration + 's, 0.01s';
-		back.setAttribute('style', styleBack);
-		back.innerHTML = characters[nextSlide][i] || ' ';
-		var char = document.createElement("span");
-		char.className = "char";
-		char.appendChild(front)
-		char.appendChild(back);
-		fragment.appendChild(char);
-	}
+// // ********************************************************
+// // NAV BAR
+// // 
+// $(function(){
+//     // Returns width of browser viewport
+//     var browser = $(window).width();
+//     // Returns width of HTML document
+//     var document = $(document).width();
+//     $('.nav .toggle-nav').click(function(e){
+//         $('.nav .nav-mobile').addClass('style-mobile').slideToggle('slow');
+//         e.preventDefault();
+//     });
+// });
+// // ------------------------------
+// // SMOOTH SCROLL
+// // ------------------------------
+// function scrollNav() {
+//     $('.nav a').click(function () {
+//         //Toggle Class
+//         $(".active").removeClass("active");
+//         $(this).closest('li').addClass("active");
+//         var theClass = $(this).attr("class");
+//         $('.' + theClass).parent('li').addClass('active');
+//         //Animate
+//         $('html, body').stop().animate({
+//             scrollTop: $($(this).attr('href')).offset().top - 160
+//         }, 400);
+//         return false;
+//     });
+//     $('.scrollTop a').scrollTop();
+// }
+// scrollNav();
+// // ------------------------------
+// // SMOOTH SCROLL END
+// // ------------------------------
+// 1000 = 1 second
+function toggleDiv() {
+	// ----------------------------------------------------------------- FIRST DIV--------------------------------------
+	//	----------------- name ----------
 	setTimeout(function () {
-		callback();
-	}, maxChars * (config.speed / config.spread) + config.speed);
-	slides[0].innerHTML = "";
-	slides[0].appendChild(fragment);
-};
-slides = document.getElementsByClassName('slide');
-currentSlide = 0;
-nextSlide = 1;
-formatText();
-// ********************************************************
-// ------------------------------
-// SMOOTH SCROLL
-// ------------------------------
-function scrollNav() {
-	$('.nav a').click(function () {
-		//Toggle Class
-		$(".active").removeClass("active");
-		$(this).closest('li').addClass("active");
-		var theClass = $(this).attr("class");
-		$('.' + theClass).parent('li').addClass('active');
-		//Animate
-		$('html, body').stop().animate({
-			scrollTop: $($(this).attr('href')).offset().top - 160
-		}, 400);
-		return false;
-	});
-	$('.scrollTop a').scrollTop();
+		$("#first_a").show(); //when the div will show
+		setTimeout(function () {
+			$("#first_a").hide(); //when the div will hide
+			toggleDiv();
+		}, 31000); //how long its visible in terms of seconds
+	}, 6000); //how long its not visible in terms of seconds
+	//	----------------- icon ----------
+	setTimeout(function () {
+		$("#first").show(); //when the div will show
+		setTimeout(function () {
+			$("#first").hide(); //when the div will hide
+			toggleDiv();
+		}, 30000); //how long its visible in terms of seconds
+	}, 7000); //how long its not visible in terms of seconds
+	// ----------------------------------------------------------------- SECOND DIV--------------------------------------
+	//	----------------- name ----------
+	setTimeout(function () {
+		$("#second_a").show(); //when the div will show
+		setTimeout(function () {
+			$("#second_a").hide(); //when the div will hide
+			toggleDiv();
+		}, 28000); //how long its visible in terms of seconds
+	}, 9000); //how long its not visible in terms of seconds
+	//	----------------- icon ----------
+	setTimeout(function () {
+		$("#second").show(); //when the div will show
+		setTimeout(function () {
+			$("#second").hide(); //when the div will hide
+			toggleDiv();
+		}, 25000); //how long its visible in terms of seconds
+	}, 10000); //how long its not visible in terms of seconds
+	// ------------------------------------------------------------------- THIRD DIV --------------------------------------------
+	//	----------------- name ----------
+	setTimeout(function () {
+		$("#third_a").show(); //when the div will show
+		setTimeout(function () {
+			$("#third_a").hide(); //when the div will hide
+			toggleDiv();
+		}, 21000); //how long its visible in terms of seconds
+	}, 14000); //how long its not visible in terms of seconds
+	//	----------------- icon ----------
+	setTimeout(function () {
+		$("#third").show(); //when the div will show
+		setTimeout(function () {
+			$("#third").hide(); //when the div will hide
+			toggleDiv();
+		}, 20000); //how long its visible in terms of seconds
+	}, 15000); //how long its not visible in terms of seconds
+	// -------------------------------------------------------------------- FOURTH DIV ----------------------------------------------
+	//	----------------- bar graph ----------
+	setTimeout(function () {
+		$("#fourth").show(); //when the div will show
+		setTimeout(function () {
+			$("#fourth").hide(); //when the div will hide
+			toggleDiv();
+		}, 15000); //how long its visible in terms of seconds
+	}, 20000); //how long its not visible in terms of seconds
+	// ----------------------------------------- THIRD DIV --------------------------------
+	//	----------------- DATA name ----------
+	setTimeout(function () {
+		$("#fifth").show(); //when the div will show
+		setTimeout(function () {
+			$("#fifth").hide(); //when the div will hide
+			toggleDiv();
+		}, 10000); //how long its visible in terms of seconds
+	}, 25000); //how long its not visible in terms of seconds
 }
-scrollNav();
-// ------------------------------
-// SMOOTH SCROLL END
-// ------------------------------
-// ********************************************************
-// NAV BAR
-// 
-$(function () {
-	// Returns width of browser viewport
-	var browser = $(window).width();
-	// Returns width of HTML document
-	var document = $(document).width();
-	$('.nav .toggle-nav').click(function (e) {
-		$('.nav .nav-mobile').addClass('style-mobile').slideToggle('slow');
-		e.preventDefault();
-	});
-});
-$(window).scroll(function () {
-	var scroll = $(window).scrollTop();
-	if (scroll >= 50) {
-		$("nav").addClass("scrolling");
-	}
-	else {
-		$("nav").removeClass("scrolling");
-	}
-});
+toggleDiv();
